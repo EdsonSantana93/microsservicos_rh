cdcbe4cf0ac126a8a5fd8dae3c0f465ccbae4060
EdsonSantana93 <edson.fs193@gmail.com>
2025-10-16 12:39:36 -0300

    Comunicação entre hr-worker e hr-payment com FeignClient

--- hr-payroll/src/main/java/com/edsoncorp/hr_payroll/service/PaymentServiceImpl.java
+++ hr-payroll/src/main/java/com/edsoncorp/hr_payroll/service/PaymentServiceImpl.java
@@ -1,32 +1,22 @@
 package com.edsoncorp.hr_payroll.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import com.edsoncorp.hr_payroll.feignclients.WorkerFeignCliente;
 import com.edsoncorp.hr_payroll.model.entities.Payment;
 import com.edsoncorp.hr_payroll.model.entities.Worker;
 
 @Service
 public class PaymentServiceImpl implements IPayment {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
-
+	private WorkerFeignCliente workerFeignCliente;
+	
+	
 	@Override
 	public Payment getPayment(Long workerId, Integer days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", String.valueOf(workerId));
-		String uri = workerHost.concat("/workers/{id}");
-		
-		Worker worker = restTemplate.getForObject(uri, Worker.class, uriVariables);
+		Worker worker = workerFeignCliente.findById(workerId).getBody();
 		Payment payment = new Payment(worker.getName(), worker.getDailyIncome(), days);
 		return payment;
 	}
